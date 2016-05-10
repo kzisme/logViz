f4967918fced645a59d32ad9ffca53e4d5b2f0df
Kevin <bleedgreenandgold@gmail.com>
2016-05-10 03:15:57 -0400

    Adding username tracking

--- script.py
+++ script.py
@@ -1,13 +1,16 @@
 import fileinput
 
 
-for line in fileinput.input():
-    if '<' in line:
-        prefix, sep, inner = line.partition('<');
-        inner, sep, suffix = inner.rpartition('>')
-        if line.partition(">")[1] == '>':
+with open('usernames.txt', 'a') as file:
+    for line in fileinput.input():
+        if '<' in line:
             prefix, sep, inner = line.partition('<');
             inner, sep, suffix = inner.rpartition('>')
-        print inner.partition('>')[0]
+            if line.partition(">")[1] == '>':
+                prefix, sep, inner = line.partition('<');
+                inner, sep, suffix = inner.rpartition('>')
+            usernames = inner.partition('>')[0];
+            for user in usernames:
+                file.write("%s\n" %usernames);
 
-
+         
