12c56d4be507cfd07e9747aef8c0c256516303dc
Kevin <bleedgreenandgold@gmail.com>
2016-05-10 23:19:43 -0400

    updating

--- script.py
+++ script.py
@@ -1,23 +1,36 @@
+import json
 import fileinput
 
+# Descriptive names are better than type names. On Python 2 you'd have overwritten the `file` global (builtin) class.
+# We avoid "with" usage here because the file is long-lived; no need to indent absolutely every other line just to
+# have an open file.  Using "with" for that is optimized for small snippets of code that open files, i.e. just to
+# write an existing variable out, then close.
+usernames = open('usernames.txt', 'a')
 
-with open('usernames.txt', 'a') as file:
-    for line in fileinput.input():
-        if '<' in line:
-            prefix, sep, inner = line.partition('<');
-            inner, sep, suffix = inner.rpartition('>')
-            if line.partition(">")[1] == '>':
-                prefix, sep, inner = line.partition('<');
-                inner, sep, suffix = inner.rpartition('>')
-            username = inner.partition('>')[0]
-            for user in username:
-                file.write("\n".join(user));
-count = {}
-for w in open('usernames.txt').read().split():
-    if w in count:
-        count[w] += 1
-    else:
-        count[w] = 1
-#for word, times in count.items():
-#    print "%s was found %d times" % (word, times)
+# If a name is still pretty generic, comments are good to add. Many editors will show comments and docstrings in
+# completion listss or help tooltips and such.  (Pymode in Vim does. ;)
+count = {}  # Track the number of occurances of each unique username.
+
+for line in fileinput.input():
+    # Exit early if this line is missing a username.  Again, saves indenting every other line.
+    if '<' not in line or '>' not in line:
+        continue
+    
+    # Separate the username from the rest of the line.
+    prefix, sep, username = line.partition('<')
+    username, sep, suffix = username.partition('>')
+
+    strippedUsername = username.strip('~!@+%&$_').lstrip()
+
+    # Track occurances.
+    count.setdefault(strippedUsername, 0)
+    count[strippedUsername] += 1
+    
+    # Record the occurance.
+    usernames.write(strippedUsername + "\n")
+
+#print(json.dumps(count))
+with open('data.json', 'w') as outfile:
+    json.dump(count, outfile,indent = 4)
+    placeid = count['kzisme']
 
