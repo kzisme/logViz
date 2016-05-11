bd7a821576f41f16d5e10356bfcb4e6541204b59
Kevin <bleedgreenandgold@gmail.com>
2016-05-11 18:50:51 -0400

    JSON Formatting

--- script.py
+++ script.py
@@ -31,5 +31,7 @@ for line in fileinput.input():
 
 #print(json.dumps(count))
 with open('data.json', 'w') as outfile:
-    json.dump(count, outfile,indent = 4)
-    print(json.dumps(list(count.items())))
+    #json.dump(count, outfile,indent = 4)
+    #print(json.dumps(list(count.items())))
+    print json.dump([dict(val=val, name=name) for (name, val) in
+        count.items()],outfile)
